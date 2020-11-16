575dd34618c44acc42342955f3f03aaa015fc820
Suhila Ahmed <suhila.ahmed@pixelogicmedia.com>
2020-11-16 18:29:27 +0200

    - change response entity

--- src/main/java/com/company/xite/equation_calculator/calculator/CalculatorController.java
+++ src/main/java/com/company/xite/equation_calculator/calculator/CalculatorController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Map;
 
@@ -17,14 +18,13 @@ public class CalculatorController {
     @Autowired
     private CalculatorService calculatorService;
 
-    @RequestMapping(method = RequestMethod.POST, value ="/calculate/{userId}")
-    public ResponseEntity<Object> calculate(@PathVariable long userId, @RequestBody Map<String, Object> equation){
+    @RequestMapping(method = RequestMethod.POST, value = "/calculate/{userId}")
+    public ResponseEntity<Object> calculate(@PathVariable long userId, @RequestBody Map<String, Object> equation) {
         try {
             Equation formattedEquation = equationService.getEquation((String) equation.get("equation"));
             return ResponseEntity.ok(calculatorService.performEquation(formattedEquation, userId));
-        }catch (Exception e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Incorrect equation format");
         }
     }
 }
